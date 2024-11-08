0c1237e09098aa4859b3f01cfc89f0a66e1cc4f7
Abdelrahman Khalifa <87104141+AbdoAhmed-Khalifa@users.noreply.github.com>
2024-11-08 15:00:43 +0200

    framer mothion

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -9,18 +9,18 @@ import Button from '../components/Button';
 import abdelrahman from '/assets/abdelrahman.pdf';
 import { AnimatedText } from '../components/AnimatedText';
 
-const HeroCamerac = lazy(() => import('../components/HeroCamerac'));
-const Targetc = lazy(() => import('../components/Targetc'));
-const ReactLogoc = lazy(() => import('../components/ReactLogoc'));
-const Cubec = lazy(() => import('../components/Cubec'));
-const Ringsc = lazy(() => import('../components/Ringc'));
-const HackerRoomc = lazy(() => import('../components/HackerRoomc'));
+const HeroCamera = lazy(() => import('../components/HeroCamera'));
+const Target = lazy(() => import('../components/Target'));
+const ReactLogo = lazy(() => import('../components/ReactLogo'));
+const Cube = lazy(() => import('../components/Cube'));
+const Rings = lazy(() => import('../components/Ring'));
+const HackerRoom = lazy(() => import('../components/HackerRoom'));
 
 export default function Hero() {
   const isSmall = useMediaQuery({ maxWidth: 440 });
   const isMobile = useMediaQuery({ maxWidth: 768 });
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
-  // const Mobile = window.screen.width <= 768;
+  const Mobile = window.screen.width <= 768;
 
   const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
@@ -41,27 +41,7 @@ export default function Hero() {
       </div>
 
       <div className="w-full h-full absolute inset-0 sm:mt-8">
-        <Canvas className="w-full h-full">
-          <Suspense fallback={<CanvasLoader />}>
-            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
-            <HeroCamerac isMobile={isMobile}>
-              <HackerRoomc
-                scale={sizes.deskScale}
-                position={sizes.deskPosition}
-                rotation={[0, -Math.PI, 0]}
-              />
-            </HeroCamerac>
-            <group>
-              <Targetc position={sizes.targetPosition} />
-              <ReactLogoc position={sizes.reactLogoPosition} />
-              <Cubec position={sizes.cubePosition} />
-              <Ringsc position={sizes.ringPosition} />
-            </group>
-            <ambientLight intensity={1} />
-            <directionalLight position={[10, 10, 10]} intensity={0.5} />
-          </Suspense>
-        </Canvas>
-        {/* {Mobile ? (
+        {Mobile ? (
           <div className="relative min-h-screen flex items-center justify-center flex-col ">
             <img
               src="/assets/hero.png"
@@ -74,24 +54,24 @@ export default function Hero() {
           <Canvas className="w-full h-full">
             <Suspense fallback={<CanvasLoader />}>
               <PerspectiveCamera makeDefault position={[0, 0, 20]} />
-              <HeroCamerac isMobile={isMobile}>
-                <HackerRoomc
+              <HeroCamera isMobile={isMobile}>
+                <HackerRoom
                   scale={sizes.deskScale}
                   position={sizes.deskPosition}
                   rotation={[0, -Math.PI, 0]}
                 />
-              </HeroCamerac>
+              </HeroCamera>
               <group>
-                <Targetc position={sizes.targetPosition} />
-                <ReactLogoc position={sizes.reactLogoPosition} />
-                <Cubec position={sizes.cubePosition} />
-                <Ringsc position={sizes.ringPosition} />
+                <Target position={sizes.targetPosition} />
+                <ReactLogo position={sizes.reactLogoPosition} />
+                <Cube position={sizes.cubePosition} />
+                <Rings position={sizes.ringPosition} />
               </group>
               <ambientLight intensity={1} />
               <directionalLight position={[10, 10, 10]} intensity={0.5} />
             </Suspense>
           </Canvas>
-        )} */}
+        )}
       </div>
 
       <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
