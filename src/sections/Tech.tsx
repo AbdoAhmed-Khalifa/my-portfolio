3250280a6f30d87d930a879aba4ded0a4da81348
Abdelrahamn <abdelrahmanahmedkhalifa99@gmail.com>
2024-12-28 21:05:18 +0200

    reomve tech anation

--- src/sections/Tech.tsx
+++ src/sections/Tech.tsx
@@ -7,14 +7,12 @@ const Tech = () => {
       <p className="head-text">Technologies I Use</p>
       <div className="flex flex-row flex-wrap justify-center sm:gap-10 gap-7 mt-12">
         {technologies.map(technology => (
-          <motion.div
-            whileHover={{ scale: 1.2 }}
-            whileTap={{ scale: 0.8 }}
+          <div
             className=" tech-logo-tech"
             key={technology.name}
           >
             <img loading="lazy" src={technology.icon} alt={technology.name} />
-          </motion.div>
+          </div>
         ))}
       </div>
     </section>
