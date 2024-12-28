7e47f214f3232f291236f426fa5986f0bc36c8dd
Abdelrahamn <abdelrahmanahmedkhalifa99@gmail.com>
2024-12-28 21:05:41 +0200

    reomve tech anation

--- src/sections/Tech.tsx
+++ src/sections/Tech.tsx
@@ -1,5 +1,4 @@
 import { technologies } from '../constants';
-import { motion } from 'framer-motion';
 
 const Tech = () => {
   return (
@@ -7,10 +6,7 @@ const Tech = () => {
       <p className="head-text">Technologies I Use</p>
       <div className="flex flex-row flex-wrap justify-center sm:gap-10 gap-7 mt-12">
         {technologies.map(technology => (
-          <div
-            className=" tech-logo-tech"
-            key={technology.name}
-          >
+          <div className=" tech-logo-tech" key={technology.name}>
             <img loading="lazy" src={technology.icon} alt={technology.name} />
           </div>
         ))}
